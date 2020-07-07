ab975015ead26be999ffdff6c28384eb1723ba6e
szintaik <szintaik@gmail.com>
2020-07-07 17:56:31 +0200

    First commit

---
+++ src/main/java/com/szintaik/awsdemo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.szintaik.awsdemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+
+        return ResponseEntity.ok().body("Helloka");
+    }
+
+
+
+
+}
